446a411e18de381c5f2ccb05ee61d2bf73dbc4e5
Geigle <JG312551@davenportschools.org>
2019-09-06 09:52:37 -0500

    update

--- src/randomGuessMatch.java
+++ src/randomGuessMatch.java
@@ -4,18 +4,16 @@ public class randomGuessMatch {
     public static void main(String[] args){
        int answer = 1 + (int)(Math.random()* 5);
        int guess;
-       boolean yes;
+
 
 
        String Sguess= JOptionPane.showInputDialog(null," pick a number between 1 and 5. ",
        "log", JOptionPane.INFORMATION_MESSAGE);
         guess = Integer.parseInt(Sguess);
+        boolean equalTo = answer == guess;
 
 
-        if( guess == answer)
-        JOptionPane.showMessageDialog(null, "yes") ;
-//        else
-//        JOptionPane.showMessageDialog(null,"the no.") ;
+        JOptionPane.showMessageDialog(null,"Is it equal?. " + equalTo) ;
 
 
 
